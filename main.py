6d915efade978e6de5ac8397e8533e969aae4e4a
Alpay Alın <alpay.aln@insider.local>
2021-09-30 13:39:21 +0300

    Secondly

--- main.py
+++ main.py
@@ -1,37 +1,65 @@
-#While loop basics with Alpay 09/29/21
+#Learning Basics with Alpay ^-^
+#I'll start with "for" loops today. 09/29/21
 
-index = 0
-list = (1, 2, 3, 4, 5)
+list = (1, 2, 3, 4, 5, 6, 7, 8)
+total = 0
 
-while (index < len(list)):
+#Basically, on each turn elements take one number, end finishes the loop.
 
-    index = index + 1
-#5 times it will be repeated.
+for element in list:
+    total = total + element
 
-    print(index)
+print(total)
 
-#-----------------------------------------------------------------------------#
+#----------------------------------------------------------------------------------#
 
-#range basics with Alpay 09/29/21
+list = (1, 2, 3, 4, 5, 6, 7, 8)
+total = 0
 
-print(*range(0,20))
+for element in list:
+    total = total + element
 
-print(*range(0, 100, 2))
+#We will be able to see what number element takes, and what number total gets on each turn and at last, the subtotal.
+
+    print("total:{} , element:{}" .format(total,element))
+
+print(total)
+
+#----------------------------------------------------------------------------------#
+
+str = "AlpayAllen"
+
+for i in str:
+
+#On python strings doesnt work so complicated as it is on C programming
+
+    print("{}" .format(i))
+    print(i * 5)
+
+#----------------------------------------------------------------------------------#
+
+list = [(1,2), (3,4), (5,6), (7,8)]
+
+for (first, second) in list:
+
+    print(first, second)
+
+#For example, it takes first and second at the same time on each turn because, it's 2 dimensional list.
+
+#----------------------------------------------------------------------------------#
+
+#DICTIONARY FIRST
+
+dictionary = {"one":1, "two":2, "three":3, "four":4, "five":5}
+
+dictionary.keys()
+
+#DICTIONARY SECOND
+
+dictionary.values()
+
+#DICTIONARY THIRD
+
+dictionary.items()
 
 
-for i in range(1, 20):
-
-    print(i)
-
-#-----------------------------------------------------------------------------#
-
-#Its going to be about listing, with Alpay.
-
-list1 = [1, 2, 3, 4, 5]
-list2 =  list () #lets add just a list
-
-for element in list1:
-
-    list2.append(element)
-
-print(list1)
